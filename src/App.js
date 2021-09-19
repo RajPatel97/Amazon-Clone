d8d4325c811e7da449338d8aeebecd6cabd6567d
RajPatel <raj191@live.com>
2021-09-18 21:49:33 -0400

    adding the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
     <div className="App">
+
     <Switch>
       <Route path = "/checkout">
         <h1>Checkout</h1>
@@ -13,6 +15,7 @@ function App() {
         <h1>Login Page</h1>
       </Route>
       <Route path = "/">
+      <Header/>
         <h1>Home Page!</h1>
       </Route>
     </Switch>
---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+
+const Header = () => {
+    return ( 
+        <nav className = "header">
+        <Link to = "/">
+        <img className = "header__logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+        </Link>
+        <div className="header__search">
+            <input type='text' className="header__searchInput"/>
+            <img className = "header__searchIcon" src="https://cravegalleria.com/wp-content/themes/CraveLocationsTheme/img/searchIcon.png" alt="" />
+        </div>
+        
+        <div className="header__nav">
+        <Link to = "/login" className = 'header__link'>
+        <div className="header__option">
+            <span className = 'header__optionLineOne'>Hello</span>
+            <span className = 'header__optionLineTwo'>Sign in </span>
+        </div>
+        </Link>
+        {/* */}
+        <Link to = "/" className = 'header__link'>
+        <div className="header__option">
+            <span className = 'header__optionLineOne'>Returns</span>
+            <span className = 'header__optionLineTwo'>& Orders</span>
+        </div>
+        </Link>
+        {/* */}
+        <Link to = "/" className = 'header__link'>
+        <div className="header__option">
+            <span className = 'header__optionLineOne'>Your</span>
+            <span className = 'header__optionLineTwo'>Prime</span>
+        </div>
+        </Link>
+        {/* */}
+        <Link to = "/checkout" className = 'header__link'>
+        <div className="header__optionBasket">
+        <img className = "header__searchIcon baskeyCount" src="https://cdn-icons-png.flaticon.com/512/34/34568.png" alt="" />
+            <span className = 'header__optionLineTwo baskeyCount'>0</span>
+        </div>
+        </Link>
+
+        </div>
+
+        </nav>
+     );
+}
+ 
+export default Header;
